e056133866ac2c03292ded49a77e42e317217c3b
sena <sena@lichtlabs.org>
2024-12-22 00:31:45 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo dnf install zsh tmux neovim gh zoxide eza stow bat -y
+
+curl -L git.io/antigen > ~/antigen.zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
